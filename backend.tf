e2c6648b24fdf372a70501cc4c3963452521fbc5
Mariam <maryam.a.ahmed08@gmail.com>
2025-05-07 08:39:01 -0700

    remove the bucket name

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket  = "mybucketweek7maa"
+    bucket  = "replace here with bucket name"
     key     = "alb/terraform.state"
     region  = "us-east-1"
     encrypt = true
